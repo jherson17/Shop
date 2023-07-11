47ee595ca5251549415ec480b0519d71fa6473c0
jherson <deluquezjherson@gmail.com>
2023-07-11 18:00:17 -0500

    cree un header que todas las paginas la tenga (views/shared) y hice que no fuese valido tener casillas vacias(app/models/concerns/book.rb)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  validates :name, presence: true
+  validates :precio, presence: true
+  validates :description, presence: true
+  validates :genero, presence: true
+  validates :cantidad_de_paginas, presence: true
 end
