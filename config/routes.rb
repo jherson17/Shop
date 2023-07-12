8aebbf7bc4e5ea7b026c1302eefbfb827f29b24a
jherson <deluquezjherson@gmail.com>
2023-07-11 21:47:52 -0500

    cree con scaffold categoria

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   root "books#index"
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
