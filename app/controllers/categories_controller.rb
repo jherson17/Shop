609cbc60e2aa928e7a0b134b4df86aca5cdd3111
jherson <deluquezjherson@gmail.com>
2023-07-11 22:50:54 -0500

    cree las categorias y las organice sin show entre otras cosas

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,11 +25,10 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
+        #cambie de category a categories para que no me llevase a show si no al listado de categorias es decir el index
+        format.html { redirect_to categories_url, notice: "Category was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -38,11 +37,10 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
+
+        format.html { redirect_to categories_url, notice: "Category was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :categories
+  #muestrame tod exptuando show
+  resources :categories, except: :show
   root "books#index"
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
